43f345cf9ac0e2e277ceca77a2d6dc61c6affa44
LiviaButtons <42958830+LiviaButtons@users.noreply.github.com>
2018-10-23 10:08:57 +0200

    main-Assets
    Adding main assets and HP so far

---
+++ js/sendMail.js
@@ -0,0 +1,46 @@
+$(document).ready(function (event) {
+    $('button').on('click', function (e) {
+        let userEmail = $('#email').val();
+        let userMsg = $('#message').val();
+
+        let formIsValid = true;
+
+        if (userEmail != '' && userEmail.indexOf('@') > 0 && userEmail.indexOf('.') > 0) {
+            $('#email').removeClass('error');
+            $('#email').attr('placeholder', 'Your email');
+        } else {
+            $('#email').addClass('error');
+            $('#email').attr('placeholder', 'Error: Please write a valid email address!');
+            formIsValid = false;
+        }
+
+        if (userMsg != '') {
+            $('#message').removeClass('error');
+            $('#message').attr('placeholder', 'Your message');
+        } else {
+            $('#message').addClass('error');
+            $('#message').attr('placeholder', 'Error: Your message cannot be empty!');
+            formIsValid = false;
+        }
+
+        if (formIsValid) {
+            $.ajax ({
+                url     : '../php/sendMail.php', 
+                type    : 'POST', 
+                dataType: 'json', 
+                data: {
+                    email   : userEmail, 
+                    message : userMsg 
+                },
+                success: function (data) {
+                    console.log (data)
+                }, 
+                error: function (xhr, status, errorMsg) {
+                    $('.contactMessage').html('Whoops, there was a technical issue.');
+                },
+                complete: function (xhr, status) {
+                }
+            });
+        };    
+    });
+});
---
+++ php/sendMail.php
@@ -0,0 +1,30 @@
+<?php
+header ('Cache-Control: no-cache');
+header ('Content-Type: application/json');
+
+if (isset ($_POST['email'], $_POST['message'])) {
+    if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) !== false && $_POST['message'] |= '') {
+        $email = $_POST['email'];
+        $message = trim(strip_tags($_POST['message']));
+        $recipient = 'contact@weeklys.com';
+        $subject = 'Weekly\'s - new message from contact form';
+        $body = 'The following person tried to contact you: ' . $email . '. This is their message: ' . $message;
+        
+        if (@mail ($recipient, $subject, $body)) {
+            echo '{"result": "OK"}';
+        } else {
+            // error: technical problem of Daemon Mail or Apache server
+            echo '{"result"; "Error"}';
+        }
+        
+    } else {
+        // error: bad values received in post
+        echo '{"result": "Error"}';
+    }
+    
+} else {
+    // error: nothing received in post
+    echo '{"result": "Error"}';
+}
+
+?>
