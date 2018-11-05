7f9f7f56cfa2246d9c80807888fb19d01866bae7
Livia Bottoni <42958830+LiviaButtons@users.noreply.github.com>
2018-11-05 12:04:20 +0100

    mobile menu now closes on scroll

--- js/headerNav.js
+++ js/headerNav.js
@@ -6,33 +6,36 @@ var navbarHeight = 90; // set in CSS
 //var navbarHeight = $('header').outerHeight();
 //console.log ($('header').outerHeight());
 
-$(window).scroll (function (event) {
+$(window).scroll(function (event) {
     didScroll = true;
 });
 
 // Check every 250ms whether a scroll did take place, only launching the function if it did
-setInterval (function () {
+setInterval(function () {
     if (didScroll) {
         hasScrolled();
         didScroll = false;
     }
 }, 250);
 
-function hasScrolled () {
+function hasScrolled() {
     var st = $(this).scrollTop();
     
     // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
+    if (Math.abs(lastScrollTop - st) <= delta) {
         return;
+    }
     
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
+    if (st > lastScrollTop && st > navbarHeight) {
         // Scroll Down
         $('header').removeClass('nav-down').addClass('nav-up');
+        $('nav').addClass('mobileHidden');
+        $('#hamMenu > span').removeClass('fa-times').addClass('fa-bars');
     } else {
         // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
+        if (st + $(window).height() < $(document).height()) {
             $('header').removeClass('nav-up').addClass('nav-down');
         }
     }
