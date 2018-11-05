92547e52dce7f97932b6a6205e968eff52812078
Livia Bottoni <42958830+LiviaButtons@users.noreply.github.com>
2018-11-05 16:20:17 +0100

    slide effect on mobile calendar v1

--- js/mobileCalendar.js
+++ js/mobileCalendar.js
@@ -1,5 +1,5 @@
 $(document).ready (function (ev) {
-    $('.jsLeftButton').on('click', function (e) {
+    $('.jsLeftButton').on('click', function (e) {    
         // target the grandparent of the button that was clicked
         // and get the index of the clicked element
         $parent = $(this).parent().parent();
@@ -7,35 +7,35 @@ $(document).ready (function (ev) {
         $minusIndex = $index - 1;
 
         // hide all calendar sections
-        $('#weeklyCalendar').children().addClass ('mobileHidden');
+        $('#weeklyCalendar').children().hide();
         
         // if index is smaller than 0 (AKA you're on Monday and going back)
         if ($minusIndex < 0) {
             $minusIndex = 6;
-            $('#weeklyCalendar > .calendarSection:nth-of-type(7)').removeClass('mobileHidden');
+            $('#weeklyCalendar > .calendarSection:nth-of-type(7)').show('slide', { direction: "left" }, 1000);
+            $('#weeklyCalendar > .calendarSection:nth-of-type(1)').hide('slide', { direction: "left" }, 1000);
         }
         
         // Show the previous calendar section
-        $parent.prev().removeClass('mobileHidden');
-
+        $parent.prev().show('slide', {direction: 'left' }, 1000);
     });
     
-    $('.jsRightButton').on('click', function (event) {
-        //target grandparent, get index
+    $('.jsRightButton').on('click', function (event) {        
         $parent = $(this).parent().parent();
         $index = $parent.index();
         $plusIndex = $index + 1;
         
-        // hida all calendar sections
-        $('#weeklyCalendar').children().addClass ('mobileHidden');
+        // hide all calendar sections
+        $('#weeklyCalendar').children().hide();
         
         // if index is bigger than 6 (AKA you're on Sunday and going forward)
         if ($plusIndex > 6) {
             $plusIndex = 0;
-            $('#weeklyCalendar > .calendarSection:nth-of-type(1)').removeClass('mobileHidden');
+            $('#weeklyCalendar > .calendarSection:nth-of-type(1)').show('slide', { direction: 'right' }, 1000);
+            $('#weeklyCalendar > .calendarSection:nth-of-type(7)').hide('slide', { direction: 'right' }, 1000);            
         }
         
         // Show the next calendar section
-        $parent.next().removeClass('mobileHidden');
+        $parent.next().show('slide', { direction: 'right' }, 1000);
     });
 });
