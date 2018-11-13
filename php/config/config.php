154f2eba13703fed05aae62203d2a32161b53882
Livia Bottoni <42958830+LiviaButtons@users.noreply.github.com>
2018-11-13 15:46:01 +0100

    user created, config updated

--- php/config/config.php
+++ php/config/config.php
@@ -4,8 +4,8 @@ ini_set('error_reporting', E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 
 define ('DB_HOST', 'localhost');
-define ('DB_USER', 'root');
-define ('DB_PWD', '');
+define ('DB_USER', 'weeklyUser');
+define ('DB_PWD', '12345');
 define ('DB_NAME', 'cook');
 define ('MYSQL_DSN', 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8');
 
