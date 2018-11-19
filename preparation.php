4b4614a9bff29364e9842d09f1943cf9a3e7001a
codecsa <studieslsfb@gmail.com>
2018-11-19 10:06:18 +0100

    rectification de l'input serving en css

--- preparation.php
+++ preparation.php
@@ -225,8 +225,8 @@ if (isset($_GET['code'])){
                 
                 <div id="portion" class="noprint">
                    <form action="" method="post">
-                       
-                        Portions : 
+                        
+                        Portions :
                         <input class ="inpServ" name='inputServing' id="inputServing" type="number" value="<?php echo $ingredients[0]['serving']?>" min="0" max="100" >
                         
                         <button id="btnServing">Calculer</button>
@@ -257,7 +257,39 @@ if (isset($_GET['code'])){
             </section>
         </div>
     </main>
-<?php include('./headFoot/footer.php')?>
+<footer class="noprint">
+    <div>
+        <div>
+            <div class="socialButtons">
+                <p>Retrouvez-nous en ligne!</p> 
+
+                <div>
+                    <span class="fab fa-facebook-square fa-2x"></span>
+                    <span class="fab fa-twitter-square fa-2x"></span>
+                    <span class="fab fa-pinterest-square fa-2x"></span>
+                    <span class="fab fa-instagram fa-2x"></span>
+                    <span class="fab fa-youtube fa-2x"></span>
+                </div>
+            </div>
+
+            <div class="contactForm">
+                <div>
+                    <label for="email" class="hidden">Adresse mail</label>
+                    <input type="text" name="email" id="email" placeholder="Adresse mail">
+                    <label for="message" class="hidden">Message</label>
+                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+                    <button id="contactButton" class="contactButton"><span class="far fa-envelope"></span> Contactez-nous</button>
+                </div>
+                <p class="contactMessage"></p>
+            </div>
+        </div>
+        <p class="copyright">&copy; Weekly's 2018</p>
+    </div>
+</footer>
+    
+    
+    
+    
 <script>
 $(document).ready(function(){
     $('#btnServing').on("click", function(e){
