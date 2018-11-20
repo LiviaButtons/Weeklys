465b51e43231cf53efdcd8243e0f4c4388ad6141
Livia Bottoni <42958830+LiviaButtons@users.noreply.github.com>
2018-11-20 09:20:11 +0100

    added close button and styles

--- js/mobileFilter.js
+++ js/mobileFilter.js
@@ -7,49 +7,75 @@ $(document).ready(function (event) {
 //    console.log ($width);
     
     if ($width < 950) {
+        // create a button, style it, add it after h2
         $button = document.createElement ('button');
         $($button).addClass ('filterButton');
-//        $text = 'Filtrer';
+        $($button).css({
+            'background-color': '#869e40',
+            'height': '40px',
+            'border': '1px solid transparent',
+            'border-radius': '5px',
+            'font-family': 'LatoBold',
+            'text-transform': 'uppercase',
+            'color': '#FFF',
+            'width': '50%'
+        });
         $($button).html('Filtrer');
         $($button).appendTo('.un');
         
+        
+        
+        // hide section 1, make section 2 larger
         $('#section1').hide('slide', {direction: 'left' }, 300);
         $('#section2').css({
-//            'display': 'flex',
             'width': '90%',
             'margin-left': 'auto',
             'margin-right': 'auto'            
         });
         
+        // when clicking onto the button, alter section 1's CSS and show it
+        // better: apply a class which is styled in mobile CSS
         $($button).on('click', function (e) {
             $('#section1').css({
                'z-index': 999,
                 'position': 'fixed',
-                'top': 80,
-                'left': '10%',
-                'width': '80%',
+                'top': 0,
+                'left': '0',
+                'width': '100%',
                 'margin-left': 'auto',
                 'margin-right': 'auto'
             });
+            
+            // Create a "close button" and insert it at the top of filter
+            $newSpan = document.createElement('span');
+            $($newSpan).addClass('fas fa-times fa-2x closeFilter').css({
+                'position': 'absolute',
+                'top': '10px',
+                'right': '20px'
+            });
+            $('#section1 > div > h3:nth-of-type(1)').before($newSpan);
+            
             $('#section1').show('slide', {direction: 'left' }, 300)
         })
         
-        $children = $('#section1 > form');
-        console.log ($children); 
+        
+//        $children = $('#section1 > form');
+//        console.log ($children); 
         
         $('#section1 > div').on('click', function (ev) {
             $clicked = ev.target;
-            console.log (ev.target);
-//            if ($.inArray($clicked, $children) == -1) {
+            console.log (ev.target);  
+            
             if (ev.target == this) {
-                $('#section1').hide('slide', {direction: 'left'}, 300);
-//                
+                // hide if click didn't occur on a child (aka form or checkbox)
+                $('#section1').hide('slide', {direction: 'left'}, 300);     
             }
-
-//            
-//        }).children().click(function (eee) {
-////            console.log ('test');
-////            return false;
         });
     } 
+    
+    // add event listener to the "close button"
+    $('.closeFilter').on('click', function (ee) {
+        $('#section1').hide('slide', {direction: 'left'}, 300);
+        console.log ('test');
+    })
 });
